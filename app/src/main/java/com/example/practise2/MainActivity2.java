b8497bedc357ff8daa68fa2ad286bfdba473cd2f
Dani <mcsf19a046@pucit.edu.pk>
2021-05-19 00:42:19 -0700

    Practise2

---
+++ app/src/main/java/com/example/practise2/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.practise2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    
+}
